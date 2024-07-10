29fe8090f3916c4b8cd3ddb66b3b113f49b372fe
Vinícius Morao <a2022951012@teiacoltec.org>
2024-07-10 09:13:19 -0300

    atvd

--- Funções de Alta Ordem/Funções Matriciais.js
+++ Funções de Alta Ordem/Funções Matriciais.js
@@ -12,13 +12,39 @@ function criarMatriz(linhas, colunas) {
     }
 }
 
+function f1(x,y)
+{return x + y;}
 
+function f2(x,y)
+{return x * y}
 
+function f3(x,y) 
+{return x == y ? 1 : 0}
+
+function f4(x,y)
+{return Math.pow(x, 2)/(y+1)}
+
+function f5(x,y)
+{return x > y ? 1 : (x < y ? 5 : 0)}
 
 
 function main() {
     var nLinhas = 5, nColunas = 5;
-    var 
+    
+    var matriz = criarMatriz(nLinhas, nColunas)(f1);
+    console.table(matriz);
+
+    matriz = criarMatriz(nLinhas, nColunas)(f2);
+    console.table(matriz);
+
+    matriz = criarMatriz(nLinhas, nColunas)(f3);
+    console.table(matriz);
+
+    matriz = criarMatriz(nLinhas, nColunas)(f4);
+    console.table(matriz);
+
+    matriz = criarMatriz(nLinhas, nColunas)(f5);
+    console.table(matriz);
 
 }
 
